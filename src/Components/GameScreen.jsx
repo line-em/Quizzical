ef70707ef2a1648e1ec4483d7441570ef2c8f1d2
Line-em <alineemily@hotmail.com>
2022-06-13 12:35:07 +0100

    QuizData moved to GameScreen

--- src/Components/GameScreen.jsx
+++ src/Components/GameScreen.jsx
@@ -1,26 +1,52 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 import QuestionBox from "../Helpers/QuestionBox";
 import Confetti from "react-dom-confetti";
 
-function GameScreen(props) {
+function GameScreen({ setGameStarted, setIsLoading, customization }) {
 	const [quizError, setQuizError] = useState(false);
 	const [score, setScore] = useState(null);
 	const [disabled, setDisabled] = useState(false);
+	const [quizData, setQuizData] = useState([]);
 
-	const quizQuestions = props.quizData.map((question) => {
-		return (
-			<QuestionBox
-				disabled={disabled}
-				chooseAnswer={chooseAnswer}
-				key={question.id}
-				{...question}
-			/>
-		);
-	});
+	if (quizData.length < 0) {
+		setGameStarted(false);
+	}
+
+	useEffect(() => {
+		setIsLoading(true);
+		fetch(
+			`https://opentdb.com/api.php?amount=${customization[0]}&category=${customization[1]}&type=multiple&difficulty=${customization[2]}&encode=base64`
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				setQuizData(
+					data.results.map((quiz) => {
+						return {
+							question: atob(quiz.question),
+							all_answers: [
+								atob(quiz.correct_answer),
+								...quiz.incorrect_answers.map((ans) => atob(ans))
+							].sort(() => Math.random() - 0.5),
+							correct_answer: atob(quiz.correct_answer),
+							current_answer: "",
+							is_correct: null,
+							id: nanoid()
+						};
+					})
+				);
+				setIsLoading(false);
+			})
+			.catch((err) => {
+				setIsLoading(false);
+				alert("Something went wrong. Please try again.");
+				console.error(err);
+			});
+	}, [customization]);
 
 	function chooseAnswer(answer, targetId) {
-		props.setQuizData(
-			props.quizData.map((question) => {
+		setQuizData(
+			quizData.map((question) => {
 				if (question.id === targetId) {
 					return {
 						...question,
@@ -35,8 +61,8 @@ function GameScreen(props) {
 	}
 
 	function checkAnswers() {
-		if (props.quizData.every((question) => question.is_correct !== null)) {
-			setScore(props.quizData.filter((quiz) => quiz.is_correct).length);
+		if (quizData.every((question) => question.is_correct !== null)) {
+			setScore(quizData.filter((quiz) => quiz.is_correct).length);
 			setDisabled(true);
 			setQuizError(false);
 		} else {
@@ -45,14 +71,26 @@ function GameScreen(props) {
 	}
 
 	function playAgain() {
-		props.setQuizData([]);
-		props.startGame();
+		setQuizData([]);
+		setGameStarted(false);
 		setScore(0);
 		setDisabled(false);
+		setQuizError(false);
 	}
 
+	const quizQuestions = quizData.map((question) => {
+		return (
+			<QuestionBox
+				disabled={disabled}
+				chooseAnswer={chooseAnswer}
+				key={question.id}
+				{...question}
+			/>
+		);
+	});
+
 	const scoreStyles = {
-		backgroundColor: score === props.quizData.length ? "var(--green)" : "var(--transparency)",
+		backgroundColor: score === quizData.length ? "var(--green)" : "var(--transparency)",
 		color: "var(--white)"
 	};
 
@@ -76,18 +114,15 @@ function GameScreen(props) {
 				<h1>Quizz Time!</h1>
 			</div>
 
-			<section className="game-screen">{props.quizData && quizQuestions}</section>
+			<section className="game-screen">{quizData && quizQuestions}</section>
 			<div className="flex-center">
-				<Confetti
-					active={props.score >= props.quizData.length / 2}
-					config={confettiConfig}
-				/>
+				<Confetti active={score >= quizData.length / 2} config={confettiConfig} />
 			</div>
 			{disabled && (
 				<div className="check-answers-container" style={scoreStyles}>
 					<h2>Correct Answers:</h2>
 					<p>
-						{score} / {props.quizData.length}
+						{score} / {quizData.length}
 					</p>
 				</div>
 			)}
